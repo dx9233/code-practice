753cf198809950d3b607eec166d48b82ab22a637
dmitrylean <adam.lean.92@gmail.com>
2026-03-15 12:40:14 +0800

    add imports kotlin

---
+++ kotlin/imports.kt
@@ -0,0 +1,6 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.unit.*
